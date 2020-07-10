6b1524752166572c7ef5ad005411ea0266a5ceca
unknown <waqar.younas7564@gmail.com>
2020-07-10 12:50:47 +0500

    ADD:new senatorDesc

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,16 @@
 package com.company;
 
 
-import javafx.util.Pair;
-
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, ParseException {
         //SenatorsBiography.scrapeData();
        // Lithuania_Bot.scrapeData();
-        SenatorsNetWorth.scrapeData();
+        //SenatorsNetWorth.scrapeData();
+        SenatorBioDesc.Scrap();
 
 
     }
